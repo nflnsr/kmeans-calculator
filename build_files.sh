3d1add05bc1600e162927fc75416e33e2844ff6b
youremail@yourdomain.com <naufaln176@gmail.com>
2023-06-08 14:12:13 +0700

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " Build Start"
-python3.8 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
-python3.8 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 echo " Build End" 
